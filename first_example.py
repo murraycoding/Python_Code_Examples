8a0f724b740a225f7fb056737c612c055aa5d19d
Brian Murray <murraycoding@gmail.com>
2023-02-01 11:24:10 -0500

    Added first script
    First script to make sure this MacBook can commit to GitHib from GitHub desktop

---
+++ first_example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# basic script to just push something into GitHub from the new MacBook
+
+num = 5
+
+if num > 4:
+    print('The number is greater than 4')
+else:
+    print('The number is 4 or less')
+
+
+
